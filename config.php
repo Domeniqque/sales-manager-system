7a16013f144687253ec43fa1ec52d4f5330db12f
Domeniqque Dylluar <domeniqque@hotmail.com>
2018-06-12 06:17:04 -0400

    chore: add scripts do banco de dados

--- config.php
+++ config.php
@@ -4,7 +4,7 @@ return [
     "database" => [
         "name" => "gestao_produto",
         "username" => "root",
-        "password" => "123456",
+        "password" => "123",
         "connection" => "mysql:host=127.0.0.1",
         "options" => [
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
